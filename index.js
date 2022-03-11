62e64efa473359496103bf7d6dd69004a86ea4e6
gopropokerdev <gopropokerdev@gmail.com>
2022-03-11 16:06:11 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const Fastify = require('fastify')
+const mercurius = require('mercurius')
+
+const app = Fastify()
+
+const schema = `
+  type Query {
+    ip: String
+  }
+`
+
+const resolvers = {
+  Query: {
+    ip: async (_, { }, context) => {
+      return context.reply.request.ip
+    }
+  }
+}
+
+app.register(mercurius, {
+  schema,
+  resolvers,
+  graphiql: true
+})
+
+app.get('/ip', async function (request) {
+  return request.ip
+})
+
+app.listen(5000, '0.0.0.0')
